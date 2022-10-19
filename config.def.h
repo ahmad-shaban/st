2973cd424f43443ef3fe72e8d8171877ce6c1d68
a7med <a7med@debian.home>
2022-10-19 23:32:27 +0200

    updated color scheme

--- config.def.h
+++ config.def.h
@@ -112,33 +112,31 @@ float alpha = 0.9;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"#000000",
-	"#aa0000",
-	"#00aa00",
-	"#aa5500",
-	"#007fff",
-	"#aa00aa",
-	"#00aaaa",
-	"#aaaaaa",
-
-	/* 8 bright colors */
-	"#555555",
-	"#ff5555",
-	"#55ff55",
-	"#ffff55",
-	"#00afff",
-	"#ff55ff",
-	"#55ffff",
-	"#ffffff",
-
+    	/* 8 normal colors */
+    	[0] = "#000000", /* black   */
+    	[1] = "#ff5555", /* red     */
+    	[2] = "#50fa7b", /* green   */
+    	[3] = "#f1fa8c", /* yellow  */
+    	[4] = "#00afff", /* blue    */
+    	[5] = "#ff79c6", /* magenta */
+    	[6] = "#8be9fd", /* cyan    */
+    	[7] = "#bbbbbb", /* white   */
+    	                              
+    	/* 8 bright colors */
+    	[8]  = "#44475a", /* black   */
+    	[9]  = "#ff5555", /* red     */
+    	[10] = "#50fa7b", /* green   */
+    	[11] = "#f1fa8c", /* yellow  */
+    	[12] = "#00afff", /* blue    */
+    	[13] = "#ff79c6", /* magenta */
+    	[14] = "#8be9fd", /* cyan    */
+    	[15] = "#ffffff", /* white   */
+                                   
 	[255] = 0,
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+    	/* special colors */
+    	[256] = "#000000", /* background */
+    	[257] = "#ffffff", /* foreground */
 };
 
 
@@ -146,9 +144,9 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 15;
-unsigned int defaultbg = 0;
-unsigned int defaultcs = 15;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 0;
 
 /*
